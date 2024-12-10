37f4ca016291a9a465bd79c1802dfc0ddaa0c2de
romashevaa <149483099+romashevaa@users.noreply.github.com>
2024-12-10 12:03:23 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,16 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-    // Create a MutationObserver to monitor changes in the DOM
-    const observer = new MutationObserver(() => {
-        const applyButton = document.querySelector('button[data-automation-id="apply-button"]');
+// document.addEventListener("DOMContentLoaded", () => {
+//     // Create a MutationObserver to monitor changes in the DOM
+//     const observer = new MutationObserver(() => {
+//         const applyButton = document.querySelector('button[data-automation-id="apply-button"]');
         
-        if (applyButton) {
-            console.log('Button found:', applyButton);
-            applyButton.click(); // Emulate the click
-            console.log('Button clicked successfully.');
-            observer.disconnect(); // Stop observing after clicking
-        }
-    });
+//         if (applyButton) {
+//             console.log('Button found:', applyButton);
+//             applyButton.click(); // Emulate the click
+//             console.log('Button clicked successfully.');
+//             observer.disconnect(); // Stop observing after clicking
+//         }
+//     });
 
-    // Start observing the document body for child nodes (e.g., the button)
-    observer.observe(document.body, { childList: true, subtree: true });
-});
+//     // Start observing the document body for child nodes (e.g., the button)
+//     observer.observe(document.body, { childList: true, subtree: true });
+// });
