47a6257e368fddc14b6de4f8703abfc578f9d8ed
romashevaa <149483099+romashevaa@users.noreply.github.com>
2024-12-11 12:33:30 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     // Create a MutationObserver to monitor changes in the DOM
     const observer = new MutationObserver(() => {
         const applyButton = document.querySelector('button[data-automation-id="apply-button"]');
-        
+        const appliedButton = document.querySelector('button[data-automation-id="applied-button"]');
+
         if (applyButton) {
-            console.log('Button found:', applyButton);
+            console.log('Apply button found:', applyButton);
             applyButton.click(); // Emulate the click
-            console.log('Button clicked successfully.');
+            console.log('Apply button clicked successfully.');
             observer.disconnect(); // Stop observing after clicking
+        } else if (appliedButton) {
+            console.log('Applied button found:', appliedButton);
+
+            // Send a postMessage to the parent window to notify that the button is applied
+            window.parent.postMessage({ status: 'applied', message: 'The button has been applied.' }, '*');
+
+            observer.disconnect(); // Stop observing after notification
         }
     });
 
